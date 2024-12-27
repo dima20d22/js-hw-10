bf8d4ca5e3318aea86f4c537f66e3b19a61892a7
dima20d22 <dimasurovatka22@gmail.com>
2024-12-27 17:02:26 +0100

    module done

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,3 +1,37 @@
 import iziToast from 'izitoast';
-
 import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+const delayTime = document.querySelector('input[name="delay"]');
+const stateInput = document.querySelectorAll('input[name="state"]');
+const button = document.querySelector('button[type="submit"]');
+
+button.addEventListener('click', e => {
+  e.preventDefault();
+
+  const delay = parseInt(delayTime.value, 10) * 1000;
+  const selectedState = [...stateInput].find(radio => radio.checked)?.value;
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (selectedState === 'fulfilled') {
+        resolve(`Fulfilled promise in ${delay}ms`);
+      } else {
+        reject(`Rejected promise in ${delay}ms`);
+      }
+    }, delay);
+  });
+  promise
+    .then(message => {
+      iziToast.show({
+        message: message,
+        color: 'green',
+      });
+    })
+    .catch(error => {
+      iziToast.show({
+        message: error,
+        color: 'red',
+      });
+    });
+});
